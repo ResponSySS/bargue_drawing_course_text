69c0c040935c1cf8ad9eab479fddf21470367a5b
Sylvain Saubier <mail@sylsau.com>
2019-12-01 20:55:51 +0100

    CACA MDLOL

---
+++ make-all.sh
@@ -0,0 +1,5 @@
+pandoc source.md -s          -o text.pdf
+pandoc source.md -s -t plain -o text.txt
+pandoc source.md -s          -o text.rtf
+pandoc source.md -s          -o text.epub
+pandoc source.md -s          -o text.docx
